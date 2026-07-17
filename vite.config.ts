ec62527be386babd07b45308c14f2809bfdc524c
oscar-jarrett <oscarjarrett1999@hotmail.com>
2026-07-16 19:54:49 -0500

    working in github

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    ssr: false, // This is the magic key! It completely disables Server-Side Rendering
-  },
-  nitro: { 
-    preset: "static" 
+    ssr: false, // This is the correct way to disable SSR in Lovable's wrapper
   },
 });
